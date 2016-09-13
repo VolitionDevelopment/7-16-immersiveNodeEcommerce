51bf68279a75172bb7d78f157a7990a5c09ffccd
Robert Bunch <rob@digitalcrafts.com>
2016-09-13 12:13:47 -0400

    Update with images, cors add on app.js

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var mongoUrl = "mongodb://localhost:27017/ecommerce";
+mongoose.connect(mongoUrl);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
---
+++ front-end/js/mainController.js
@@ -0,0 +1,38 @@
+var ecommerceApp = angular.module('ecommerceApp', ['ngRoute', 'ngCookies']);
+ecommerceApp.controller('mainController', function($scope, $http, $location, $cookies){
+
+	var apiPath = "http://localhost:3000";
+
+	$scope.register = function(){
+		console.log($scope.username);
+		$http.post(apiPath + '/register', {
+			username: $scope.username,
+			password: $scope.password,
+			password2: $scope.password2,
+			email: $scope.email
+		}).then(function successCallback(response){
+			console.log(response);
+		},function errorCallback(response){
+			console.log(response);
+		});
+	};
+});
+
+//Set up routes using the routes module
+ecommerceApp.config(function($routeProvider){
+	$routeProvider.when('/',{
+		templateUrl: 'views/main.html',
+		controller: 'mainController'
+	})
+	.when('/login',{
+		templateUrl: 'views/login.html',
+		controller: 'mainController'
+	})
+	.when('/register',{
+		templateUrl: 'views/register.html',
+		controller: 'mainController'
+	}).otherwise({
+		redirectTo: '/'
+	})
+
+});
